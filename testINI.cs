d9fb5509e0856383902f75808fdd913773c8eddf
luciaSKLE <lucia.rodriguez@skylife-eng.com>
2021-01-11 15:59:08 +0100

    Create testINI.cs

---
+++ testINI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testINI : MonoBehaviour
+{
+    public testINI()
+    {
+        Debug.Log("Ha entrado en el constructor de testINI");
+    }
+    public string IP;
+    Config_Reader config;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //config = new Config_Reader();
+        config = GetComponent<Config_Reader>();
+        //config.path = @"config.ini";
+        //IP = config.read_IP();
+        Debug.Log(IP);
+    }
+
+    
+}
