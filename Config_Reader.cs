d71200953c8f1d0aedfe4305e11b8c6b95514439
luciaSKLE <lucia.rodriguez@skylife-eng.com>
2021-01-11 16:13:12 +0100

    añadir fichero config_reader.cs
    de nuevas

---
+++ Config_Reader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using UnityEngine;
+
+public class Config_Reader : MonoBehaviour
+{
+    public string IP;
+    public int port;
+    public string topics;
+
+    IniFile rd;
+
+    public Config_Reader()
+    {
+        this.IP = "";
+        this.port = 0;
+        this.topics = "";
+
+    }
+    public string read_IP()
+    {
+        //rd.path = this.path;
+        this.rd = new IniFile();
+        this.IP = "IP...";
+        //this.IP = rd.IniReadValue("Session", "IP_session");
+        return this.IP;
+    }
+
+    public int read_port()
+    {
+        //rd.path = this.path;
+        this.port = int.Parse(rd.IniReadValue("Session", "port"));
+        return this.port;
+    }
+
+    public string read_topics()
+    {
+        //rd.path = this.path;
+        this.topics = rd.IniReadValue("Topics", null);
+        return this.topics;
+    }
+
+
+}
